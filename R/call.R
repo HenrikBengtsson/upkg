b62f7de5e1416d20fc3c6469d748d957ef6876cf
hb <hb@aroma-project.org>
2016-10-15 11:27:28 -0700

    Now call/callc also takes <pkg>::<fcn>

--- R/call.R
+++ R/call.R
@@ -1,14 +1,28 @@
 #' @export
 call <- CmdArgsFunction(function(fcn, ...) {
-  fcn <- get(fcn, mode = "function")
+  fcn <- .getfcn(fcn)
   fcn(...)
 }, output = print)
 
 #' @export
 callc <- CmdArgsFunction(function(fcn, ...) {
-  fcn <- get(fcn, mode = "function")
+  fcn <- .getfcn(fcn)
   fcn(...)
 }, output = cat)
+
+
+.getfcn <- function(fcn, ...) { 
+  fcn <- as.character(fcn)
+  parts <- unlist(strsplit(fcn, split="::", fixed=TRUE))
+  str(list(fcn=fcn, parts=parts))
+  if (length(parts) == 2) {
+    envir <- getNamespace(parts[1])
+    fcn <- parts[2]
+  } else {
+    envir <- parent.frame()
+  }
+  get(fcn, envir = envir, mode = "function")
+} ## .getfcn()
 
 
 #' @importFrom R.utils isUrl downloadFile

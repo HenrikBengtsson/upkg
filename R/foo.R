8298f634a830df3cd819079e0898f18beb6e7ead
hb <hb@aroma-project.org>
2016-12-30 11:05:13 -0800

    Travis CI: How many cores do Travis CI provide?

--- R/foo.R
+++ R/foo.R
@@ -6,12 +6,3 @@
 #'
 #' @export
 foo <- function(x=1, y=2, ...) list(x=x, y=y)
-
-
-#' Bar something
-#'
-#' @inheritParams foo
-#' @param \dots Additional arguments passed to \code{\link{foo}()}.
-#'
-#' @export
-bar <- function(x=1, ...) foo(x=x, ...)
